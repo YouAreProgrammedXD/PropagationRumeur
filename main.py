ce020804434e25a4278a5545ca9d006113964406
YouAreProgrammedXD <guiffant.theophane@gmail.com>
2025-07-23 18:57:40 +0200

    version1 de la fonction génératrice de réseaux

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main(n,p):
-    reseau = np.ones((n, 1), dtype=list)
     nbinfecte = propagationrumeurs(n,p) #infecté par tour/incrément
     plt.plot(nbinfecte)
     plt.xlabel("tour/incrément")
@@ -11,63 +10,56 @@ def main(n,p):
     plt.show()
 
 
-
+#infecte.append(propagation(reseau,p,[reseau[0]],[[1]],[1]))
 
 #nbinfecte->nombre d'infecté par tour/increment; infecte-> numéro infecté actuellement
 def propagation(reseau,p,propagateurs,nbinfecte,infecte):
-    if (propagateurs==[]):
-        return(nbinfecte)
-    else:
-        print("XD")
-    return([1])
+    return(1)
 
+
+#une personne a au moins une autre qui la suis
 def propagationrumeurs(n,p):
-    reseau = np.ones((n, 1), dtype=list)
-    nbinfecte=[[1]]
-    n=len(reseau)
-    k=n-1
+    reseau = np.empty(n, dtype=object)
+    reseau[0]=[1]
+    for i in range(1,n):
+        reseau[i]=[0]
+    infecte=[]
+    k=0
+    retour=False
     while (k!=-1):
-        if (complet(reseau[k],k,n-1)):
-            reseau[k]=[]
-            k=k-1
-        else:
-            if (reseau[k]==[]):
-                if (k==0):
-                    reseau[k].append(1)
-                else:
-                    reseau[k].append(0)
+        infecte.append(propagation(reseau,p,[reseau[0]],[[1]],[1]))
+        while (complet(reseau[k],n-1)):
+            if (k==0):
+                reseau[k]=[1]
             else:
-                if (besoinajout(reseau[k],k,n)):
-                    reseau[k]=creer(len(reseau[k])+1,k)
-                else:
-                    reseau[k]=increment(reseau[k],k,n)
-            k=k+1
-            nbinfecte.append(propagation(reseau,p,[reseau[0]],[[1]],[1]))
-    return(moyenne(nbinfecte))
+                reseau[k]=[0]
+            k=k-1       
+        if (k!=-1): 
+            if (besoinajout(reseau[k],k,n)):
+                reseau[k]=creer(len(reseau[k])+1,k)
+            else:
+                reseau[k]=increment(reseau[k],k,n)
+            if (k<n-1):
+                k=k+1
+    return(moyenne(infecte))
 
 def moyenne(liste):
-    taille=len(liste)
-    total=[1]
-    i=0
-    while (total[-1]!=0):
-        t=0
-        for l in range(0,taille):
-            if (i<len(liste[l])):
-                t=t+liste[l][i]
-        total.append(t/taille)
-    del total[0]
-    del total[len(total)-1]
-    return(total)
+    L=len(liste)
+    t=0
+    for i in range(L):
+       t=t+liste[i]
+    t=t/L 
+    return(t)
 
 
-
+#si on ne peut rien incrémenter <=> les abonnées sont bloquer sur la fin de la bande disponible [0;n-1]/k
 def besoinajout(liste,k,n):
-    if ((liste[-len(liste)]==n-len(liste) and k<n-len(liste)) or (liste[-len(liste)]==n-len(liste)-1 and k>=n-len(liste))):
+    if ((liste[0]==n-len(liste) and k<liste[0]) or (liste[0]==n-len(liste)-1 and k>liste[0])):
         return(True)
     else:
         return(False)
 
-
+#créer une nouvelle liste d'abonnée de len=taille+1
 def creer(taille,k):
     if (k>=taille):
         return([i for i in range(0,taille)])
@@ -76,14 +68,20 @@ def creer(taille,k):
         liste.remove(k)
         return(liste)
 
+#on incrémente le premier numéro d'abonnée possible
 def increment(liste,k,n):
     i=1
-    while (liste[-i]==n-i or n-i==k):
+    while (liste[-i]==n-i or (liste[-i]+1==k and (k==n-1 or (i!=1 and liste[-i+1]==liste[-i]+2)))):
         i=i+1
-    liste[-i]=liste[-i]+1
+    if (liste[-i]+1==k):
+        liste[-i]=liste[-i]+2
+    else:
+        liste[-i]=liste[-i]+1
     return(liste)
 
-def complet(liste,k,taille):
+
+#si la liste d'abonné comprend tout les individus possible
+def complet(liste,taille):
     if (len(liste)!=taille):
         return(False)
     else:
@@ -92,4 +90,4 @@ def complet(liste,k,taille):
 
 
 
-main(1000,0.3)
+main(100,0.3)
